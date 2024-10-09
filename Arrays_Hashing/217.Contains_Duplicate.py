879dcea7f96d5edab1c575e0de19a49eaffd2100
zhushule <zhushule1996@gmail.com>
2024-10-09 15:07:34 -0400

    add

---
+++ Arrays_Hashing/217.Contains_Duplicate.py
@@ -0,0 +1,51 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+"""
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()  # Initialize an empty set
+        for num in nums:
+            if num in seen:  # If the number is already in the set, a duplicate exists
+                return True
+            seen.add(num)  # Add the number to the set
+        return False  # No duplicates found
